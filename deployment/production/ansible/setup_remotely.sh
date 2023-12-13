58228785c53e1a27e47e6cf1545092c97dae5e34
cahya <cahya@btech.id>
2023-12-13 21:44:26 +0700

    revise gitignore

--- deployment/production/ansible/setup_remotely.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "=== Setting up the production server ==="
-date
-
-cd "$(dirname "$0")"
-ansible-playbook setup.yml -i hosts/remote
--- deployment/production/ansible/update_remote.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "=== Updating project to production server ==="
-date
-
-cd "$(dirname "$0")"
-ansible-playbook update.yml -i hosts/remote
