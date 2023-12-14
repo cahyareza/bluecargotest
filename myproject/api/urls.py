c85bf3d7e4ebbcbee729201f2dfe4259eed1ef95
cahya <cahya@btech.id>
2023-12-14 14:08:22 +0700

    Feat(api)-delete api root

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from .views import ApiRoot
 
 app_name = "api"
 urlpatterns = [
@@ -7,5 +6,4 @@ urlpatterns = [
     path('categories/', include('myproject.api.categories.urls')),
     path('destination/', include('myproject.api.destination.urls')),
     path('calculate/', include('myproject.api.calculation.urls')),
-    path('list/', ApiRoot.as_view(), name=ApiRoot.name),
 ]
